9e3b4d108bf347ff9f0e060eaf5131939c20a12c
SergoQATester <163989336+SergoQATester@users.noreply.github.com>
2024-04-16 19:44:19 +0300

    new change

--- 27.03.2024.py
+++ 27.03.2024.py
@@ -1,10 +1,25 @@
-text = input()
-new_list = list(text)
+# text = input()
+# new_list = list(text)
 
-def count_new_list(new_list):
-  count = 0
-  for i in new_list:
-    count += 1
-  return count
-total = count_new_list(new_list)
-print(total)
+# def count_new_list(new_list):
+#   count = 0
+#   for i in new_list:
+#     count += 1
+#   return count
+# total = count_new_list(new_list)
+# print(total)
+
+spam = 40
+a = 30
+b = 70
+c = spam + a + b
+print(c)
+
+
+
+
+
+
+
+
+
